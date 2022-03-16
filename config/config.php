6acf469ecacf1445c4e7dc99b6382f8e8f5b91aa
danmauie <duenasmauie@gmail.com>
2022-03-16 13:40:45 +0800

    First Commit!

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479130');
+	define('DB_PASS', 'nl24CEmadX');
+	define('DB_NAME', 'sql6479130');
---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/JayrahBautista">Jayrah Bautista</a> for BSIT 3B1</p>
+      <p>Created by <a href="https://github.com/danmauie">Dan Mauie Dueñas</a> for BSIT 3B1</p>
+      <p>Created by <a href="https://github.com/elmerrubiojr">Elmer Rubio</a> for BSIT 3B1</p>
+    </div>
+</footer>
+  </body>
+</html>
